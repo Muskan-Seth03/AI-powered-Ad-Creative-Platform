be9f32fcd45eb77afa62bb0cf2f159a9ea5e1fdb
Muskan-Seth03 <muskanseth003@gmail.com>
2026-01-18 21:08:26 +0530

    first commit

---
+++ server/configs/instrument.mjs
@@ -0,0 +1,6 @@
+import * as Sentry from "@sentry/node";
+
+Sentry.init({
+    dsn: "https://5dca9f63f48775303d5517267d4f643c@o4510732139364352.ingest.us.sentry.io/4510732144869376",
+    sendDefaultPii: true,
+});
